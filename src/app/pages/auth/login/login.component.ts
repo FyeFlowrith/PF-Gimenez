8f1590cfdbdc26aff7e1894c3955bb4de19d1269
Romina Giménez <promina.gimenez@gmail.com>
2024-11-05 00:16:05 -0300

    update

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ export class LoginComponent {
   passwordInputType: 'password' | 'text' = "password";
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService){
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router){
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
@@ -32,10 +33,13 @@ export class LoginComponent {
     } else {
       this.authService.login(this.loginForm.value).subscribe({
         next: (result) => {
-          console.log(result);
+          this.router.navigate(['dashboard']);
         },
         error: (err) => {
           console.error(err);
+          if (err instanceof Error) {
+            alert(err.message);
+          }
         },
       });
     }
